66dd3344bdb47cd62c177470320be53b8794ed87
baya-aymen <aymenbaya@gmail.com>
2020-11-22 11:14:40 +0100

    creation du formulaire avec validation les champs: nom, prenom , telephone

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
@@ -11,5 +12,10 @@ export class FormulaireComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    
+}
 
 }
